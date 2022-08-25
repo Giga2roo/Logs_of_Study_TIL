ae7417fbf8e654e4ad1ec49f30c5e0ea9f5104ed
Giga2roo <sayhoahn@naver.com>
2022-08-25 23:18:55 +0900

    push TIL '22.08.25

---
+++ SLs_Coding/practice_module.py
@@ -0,0 +1,15 @@
+# module1.py
+#def add(a, b):
+#    return a + b
+
+def sub(a, b):
+    return a - b
+
+# module2.py
+PI = 3.141592
+
+class Math:
+    def solv(self, r):
+        return PI*(r**2)
+def add(a, b):
+    return a+b
